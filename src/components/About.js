415d9ff19beb79ad496db8b5d354ba680c2758c6
Karina Miernik <karina.miernik@gmail.com>
2022-01-15 21:46:27 +0100

    about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Title from './Title'
 import '../styles/About.css'
+import me from '../assets/me.jpeg'
 const About = (props) => {
   return(
-    <div>
+    <div className='wrapperAbout'>
       <Title title="Learn about me." />
       <div className='about-container'>
-        <div className='about'>
+      <div className='about'><div className='about-image'></div></div>
+        <div className='about-desc'>
           <p>I am Front-End Developer located in Poland. I am creative person, who always is eager to learn more.</p>
           <p>In private I am a book lover and amatour artist.</p>
           <h3>Find out more</h3>
           <a href="https://github.com/karina-miernik" target="_blank" rel="noreferrer">Github page</a>
           </div>
-        <div className='about'>My image</div>
       </div>
     </div>    
   )
