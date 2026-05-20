8d2fcc2838bcfec392923a14a55f5a11844430da
Karina Miernik <karina@gastroplanner.no>
2026-05-20 14:42:05 +0200

    update card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import '../styles/Card.css'
-const Card = ({description, name, link, image}) => {
-  return(
-    <div className='card'>
-      <div className='image-container'>
-        <img src={image} alt="Project image"/>
-      </div>
-      <div className='details-container'>
-        <div className='name'>{name}</div>
-        <div className='desc'>{description}</div>
-        <a className='project-link' href={link}>{`Visit ${name}`}</a>
-      </div>
-    </div>
-  )
-  };
+import React from "react";
+import "../styles/Card.css";
+const Card = ({ description, name, link, image }) => {
+    return (
+        <a
+            className="project-link"
+            href={link}
+            target="_blank"
+            rel="noreferrer"
+        >
+            <div className="card">
+                <div className="image-container">
+                    <img src={image} alt="" />
+                </div>
+                <div className="details-container">
+                    <div className="name">{name}</div>
+                    <div className="desc">{description}</div>
+                    {`Visit ${name}`}
+                </div>
+            </div>
+        </a>
+    );
+};
 
 export default Card;
